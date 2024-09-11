366ab615371f9bd12d92ccae3cbd3efafe375d72
abdelllatif <haissouneabdellatif749@gmail.com>
2024-09-11 12:35:51 +0100

    Update my project for sas!

--- main.c
+++ main.c
@@ -1,238 +1,368 @@
-
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
 typedef struct {
-   int jour;
-   int mois;
-   int anee;
-   }date_ness;
-
-   typedef struct {
-   int id_dep;
-   char nom_dep[40];
-   float moyenne_gÈnÈrale;
-}departement;
+    int jour;
+    int mois;
+    int annee;
+} date_ness;
 
 typedef struct {
-int NumÈro_unique;
-char Nom[30];
-char prenom[30];
-int id_DÈpartement;
-float Note_gÈnÈrale;
-date_ness date;
+    int id_dep;
+    char nom_dep[40];
+    float moyenne_generale;
+} departement;
+
+typedef struct {
+    int numero_unique;
+    char nom[30];
+    char prenom[30];
+    int id_departement;
+    float note_generale;
+    date_ness date;
 } etudiant;
+
 etudiant t[100000];
 departement v[11];
 departement copie[11];
-int dim=0;
-int dimm=0;
-int nbr;
-int i,j;
-int choix;
+int dim = 0;
+int dimm = 0;
+int i, j, nbr;
+etudiant temp;
 
-
-
-
-
-void ajouter_etud (){
-    printf("enter le id d'etudiant(unique): ");
-    scanf("%d",&t[dim].NumÈro_unique);
-    printf("enter le nom d'etudiant");
-    scanf("%29[^\n]",t[dim].Nom);
-    printf("enter le prenom d'etudiant");
-    scanf("%29[^\n]",t[dim].prenom);
-    printf("enter l'anne de nessance: ");
-    scanf("%d",&t[dim].date.anee);
-    printf("enter le mois de nessance");
-    scanf("%d",&t[dim].date.mois);
-    printf("enter le jour de nessance");
-    scanf("%d",&t[dim].date.jour);
-    printf("enter le id de deppartement");
-    scanf("%d",&t[dim].id_DÈpartement);
-    printf("enter la note generale de l'etudiant");
-    scanf("%d",&t[dim].Note_gÈnÈrale);
+void ajouter_etud() {
+    printf("Entrez l'id de l'√©tudiant (unique) : ");
+    scanf("%d", &t[dim].numero_unique);
+    printf("Entrez le nom : ");
+    scanf(" %29[^\n]", t[dim].nom);
+    printf("Entrez le pr√©nom : ");
+    scanf(" %29[^\n]", t[dim].prenom);
+    printf("Date de naissance.\n");
+    printf("Ann√©e : ");
+    scanf("%d", &t[dim].date.annee);
+    printf("Mois : ");
+    scanf("%d", &t[dim].date.mois);
+    printf("Jour : ");
+    scanf("%d", &t[dim].date.jour);
+    printf("Entrez l'id du d√©partement : ");
+    scanf("%d", &t[dim].id_departement);
+    printf("\nEntrez la note g√©n√©rale de l'√©tudiant : ");
+    scanf("%f", &t[dim].note_generale);
     dim++;
 }
 
-
-
-
-void ajouter_dep (){
-    printf("enter le id de departement(unique): ");
-    scanf("%d",&v[dimm].id_dep);
-    printf("enter le nom de departement");
-    scanf("%49[^\n]",v[dimm].nom_dep);
+void ajouter_dep() {
+    printf("\nEntrez l'id du d√©partement (unique) : ");
+    scanf("%d", &v[dimm].id_dep);
+    printf("\nEntrez le nom du d√©partement : ");
+    scanf(" %39[^\n]", v[dimm].nom_dep);
     dimm++;
 }
-void modifier_info(){
-    printf("entrer le numero d'etudiant por fair modification");
-    scanf("%d",&nbr);
-    for(i=0;i<dim;){
-        if(t[i].NumÈro_unique==nbr){
-     printf("enter le nom d'etudiant");
-    scanf("%29[^\n]",t[i].Nom);
-    printf("enter le prenom d'etudiant");
-    scanf("%29[^\n]",t[i].prenom);
-    printf("enter l'anne de nessance: ");
-    scanf("%d",&t[i].date.anee);
-    printf("enter le mois de nessance");
-    scanf("%d",&t[i].date.mois);
-    printf("enter le jour de nessance");
-    scanf("%d",&t[i].date.jour);
-    printf("enter le id de deppartement");
-    scanf("%d",&t[i].id_DÈpartement);
-    printf("enter la note generale de l'etudiant");
-    scanf("%d",&t[i].Note_gÈnÈrale);
-    break;
+
+void modifier_info() {
+    int nbr;
+    printf("\nEntrez l'id de l'√©tudiant : ");
+    scanf("%d", &nbr);
+    for (int i = 0; i < dim; i++) {
+        if (t[i].numero_unique == nbr) {
+            printf("entrez les informations de l'etudiant\n");
+            printf("Nom : ");
+            scanf(" %29[^\n]", t[i].nom);
+            printf("Pr√©nom : ");
+            scanf(" %29[^\n]", t[i].prenom);
+            printf("Date de naissance.\n");
+            printf("Ann√©e de naissance : ");
+            scanf("%d", &t[i].date.annee);
+            printf("Mois : ");
+            scanf("%d", &t[i].date.mois);
+            printf("Jour : ");
+            scanf("%d", &t[i].date.jour);
+            printf("Id du d√©partement : ");
+            scanf("%d", &t[i].id_departement);
+            printf("Entrez la note g√©n√©rale : ");
+            scanf("%f", &t[i].note_generale);
+            return;
         }
-        else
-printf("l'Ètudiant n'existe pas!");
     }
+    printf("\netudiant introuvable !\n");
 }
 
-
-
-void suprimer_info(){
-    printf("entrer le numero d'etudiant pour supprimer ");
-    scanf("%d",&nbr);
-    for(i=0;i<dim;){
-        if(t[i].NumÈro_unique==nbr){
-            t[i]=t[i+1];
-            i--;
-}
-else
-printf("l'Ètudiant n'existe pas!");
-}
-}
-
-
-
-void afficher_info(){
-    printf("entrer votre numero d'etudiant pour afficher");
-    scanf("%d",&nbr);
-    for(i=0;i<dim;){
-        if(t[i].NumÈro_unique==nbr){
-    printf(" nom: %d\n",t[i].Nom);
-    printf(" prenom: %d\n",t[i].prenom);
-    printf(" dat essence nessance: %d/%d%/d\n",t[i].date.anee,t[i].date.mois,t[i].date.jour);
-    for(j=0;j<dimm;j++){
-       if(t[i].id_DÈpartement==v[j].id_dep)
-       printf(" iscrit dans la dÈpartement de: %d\n",v[j].nom_dep);
-    }
-    printf("la note generale:",t[i].Note_gÈnÈrale);
-    break;
-
-
-}
-}
-int count=0;
-int sum=0;
-void calcule_moyyene(){
-for(i=0;i<dimm;i++){
-   for(j=0;j<dim;j++){
-    if(t[j].id_DÈpartement==v[i].id_dep){
-    sum=sum+t[j].Note_gÈnÈrale;
-    count++;
-   }
-   }
- copie[i].moyenne_gÈnÈrale=sum/count;
-}
-printf("les moyenne generale de chaque deppartement sont: \n");
-for(i=0;i<dimm;i++){
-    printf("%s : %f ",v[i].nom_dep,copie[i].moyenne_gÈnÈrale);
-    float cn=0;
-for(i=0;i<dimm;i++){
-    cn=cn+copie[i].moyenne_gÈnÈrale;
-}
-printf("la moyenne general au universitÈ est: %f\n",cn/dimm);
-}
-
-void to_lowercase(char z[]) {
-    for (int i = 0; z[i]; i++) {
-        z[i] = tolower((unsigned char)z[i]);
-    }
-}
-void chercher() {
-    char nom_cherche[30];
-    char nom_lower[30];
-    printf("vous aper le choix de recherche tapez:\n1: rechercher et afficher par nom.\n 2: aficher une list des etudiant dans une departement.\n 3: anuller le choix ");
-    scanf("%d",&choix);
-    switch (choix) {
-    case 1:
-    printf("Enter the last name of the student to search: ");
-    scanf("%29s", nom_cherche);
-    to_lowercase(nom_cherche);
-    for (i = 0; i < dim; i++) {
-strcpy(nom_lower, t[i].Nom);
-to_lowercase(nom_lower);
-if (strcmp(nom_lower, nom_cherche) == 0) {
-     printf(" nom: %d\n",t[i].Nom);
-    printf(" prenom: %d\n",t[i].prenom);
-    printf(" dat essence nessance: %d/%d%/d\n",t[i].date.anee,t[i].date.mois,t[i].date.jour);
-    for(j=0;j<dimm;j++){
-       if(t[i].id_DÈpartement==v[j].id_dep)
-       printf(" iscrit dans la dÈpartement de: %d\n",v[j].nom_dep);
-    }
-    printf("la note generale:",t[i].Note_gÈnÈrale);
-    break;
-               }
+void supprimer_info() {
+    int nbr;
+    printf("Entrez l'id de l'√©tudiant √† supprimer : ");
+    scanf("%d", &nbr);
+    for (int i = 0; i < dim; i++) {
+        if (t[i].numero_unique == nbr) {
+            for (int j = i; j < dim - 1; j++) {
+                t[j] = t[j + 1];
             }
-            printf("etudiant n'existe pas!\n");
-            break;
-        default:
-            printf("Invalid choix!\n");
-            break;
-        case 2:
-
-    printf("Enter le numero de departement: ");
-    scanf("%d",&nbr);
-    to_lowercase(nom_cherche);
-    for (i = 0; i < dimm; i++) {
-        if (t[i].id_DÈpartement==nbr) {
-             printf("les etudiant insctit dans departement de %s sont:\n",v[i].nom_dep);
-     printf("nom\tprenom\tdate de naissance\note generale\n");
-        for (j = 0; j < dim; j++) {
-if (t[i].id_DÈpartement==v[i].id_dep) {
-        printf("%s\t%s\t%d/%d/%d\t%f",t[i].Nom,t[i].prenom,t[i].date.anee,t[i].date.mois,t[i].date.jour,t[i].Note_gÈnÈrale);
-    }
-        }
+            dim--;
+            printf("suprimer avec sucsses!");
+            return;
         }
     }
-    break;
-    case 3:
-    break;
+    printf("etudiant introuvable !\n");
 }
-void afficher_par_alpha() {
-    etudiant b[100000];
-    etudiant temp;
+
+void afficher_info() {
+    int nbr;
+    printf("Entrez l'id de l'√©tudiant pour afficher les informations :\n ");
+    scanf("%d", &nbr);
     for (int i = 0; i < dim; i++) {
-        b[i] = t[i];
+        if (t[i].numero_unique == nbr) {
+            printf("Nom : %s\n", t[i].nom);
+            printf("Pr√©nom : %s\n", t[i].prenom);
+            printf("Date de naissance : %d/%d/%d\n", t[i].date.jour, t[i].date.mois, t[i].date.annee);
+            printf("D√©partement : ");
+            for (int j = 0; j < dimm; j++) {
+                if (t[i].id_departement == v[j].id_dep) {
+                    printf("  %s\n", v[j].nom_dep);
+                    break;
+                }
+            }
+            printf("Note g√©n√©rale : %.2f\n", t[i].note_generale);
+            return;
+        }
     }
+    printf("√âtudiant introuvable !\n");
+}
+
+void calcule_moyenne() {
+    int count;
+    int sum = 0;
+    count = 0;
+    for (int i = 0; i < dimm; i++) {
+        for (int j = 0; j < dim; j++) {
+            if (t[j].id_departement == v[i].id_dep) {
+                sum =sum+t[j].note_generale;
+                count++;
+            }
+        }
+        if (count > 0) {
+            v[i].moyenne_generale = sum / count;
+        }
+    }
+    printf("Moyenne g√©n√©rale des d√©partements :\n");
+    for (int i = 0; i < dimm; i++) {
+        printf("%s : %.2f\n", v[i].nom_dep, v[i].moyenne_generale);
+    }
+    float t_sum = 0;
+    int t_count = 0;
+    for (int i = 0; i < dimm; i++) {
+        t_sum += v[i].moyenne_generale;
+        t_count++;
+    }
+    printf("Moyenne g√©n√©rale de l'universit√© : %.2f\n", t_sum / t_count);
+}
+
+void afficher_statistiques() {
+    float seuil;
+    printf("Entrez le seuil de note :\n ");
+    scanf("%f", &seuil);
+    printf("Les √©tudiants ayant une note sup√©rieure ou √©gale √† %.2f :\n", seuil);
     for (int i = 0; i < dim; i++) {
-        to_lowercase(t[i].Nom);
+        if (t[i].note_generale > seuil) {
+            printf("%s %s : %.2f\n", t[i].nom, t[i].prenom, t[i].note_generale);
+        }
     }
 
+    etudiant tri_meilleur[100000];
+    for (int i = 0; i < dim; i++)
+        tri_meilleur[i] = t[i];
     for (int i = 0; i < dim - 1; i++) {
         for (int j = i + 1; j < dim; j++) {
-            if (strcmp(b[i].Nom, b[j].Nom) > 0) {
+            if (tri_meilleur[i].note_generale < tri_meilleur[j].note_generale) {
+                temp = tri_meilleur[i];
+                tri_meilleur[i] = tri_meilleur[j];
+                tri_meilleur[j] = temp;
+            }
+        }
+    }
+    printf("Les 3 meilleurs √©tudiants :\n");
+    for (int i = 0; i < 3 && i < dim; i++) {
+        printf("%s %s : %.2f\n", tri_meilleur[i].nom, tri_meilleur[i].prenom, tri_meilleur[i].note_generale);
+    }
+
+    int reussit_count;
+    printf("Nombre d'√©tudiants r√©ussis (note >= 10) :\n");
+    for (int i = 0; i < dimm; i++) {
+        reussit_count = 0;
+        for (int j = 0; j < dim; j++) {
+            if (t[j].id_departement == v[i].id_dep && t[j].note_generale >= 10) {
+                reussit_count++;
+            }
+        }
+        printf("%s : %d\n", v[i].nom_dep, reussit_count);
+    }
+}
+
+void chercher() {
+    int choix;
+    char nom_cherche[30];
+    printf("Choisissez une option de recherche :\n");
+    printf("1 : Recherche par nom.\n");
+    printf("2 : Lister les √©tudiants d'un d√©partement.\n");
+    printf("3 : Annuler\n");
+    scanf("%d", &choix);
+    switch (choix) {
+        case 1:
+            printf("Entrez le nom √† rechercher : ");
+            scanf(" %29s", nom_cherche);
+            for (int i = 0; i < dim; i++) {
+                if (strcasecmp(t[i].nom, nom_cherche) == 0) {
+                    afficher_info();
+                    return;
+                }
+            }
+            printf("√âtudiant non trouv√© !\n");
+            break;
+        case 2:
+            printf("Entrez l'id du d√©partement : ");
+            int dept_id;
+            scanf("%d", &dept_id);
+            printf("√âtudiants dans le d√©partement :\n");
+            for (int i = 0; i < dim; i++) {
+                if (t[i].id_departement == dept_id) {
+                    printf("%s %s : %.2f\n", t[i].nom, t[i].prenom, t[i].note_generale);
+                }
+            }
+            break;
+        case 3:
+            break;
+        default:
+            printf("Choix invalide !\n");
+            break;
+    }
+}
+
+void afficher_par_alpha() {
+    etudiant b[100000];
+   b[i]=t[i] ;
+    etudiant temp;
+    for (int i = 0; i < dim - 1; i++) {
+        for (int j = i + 1; j < dim; j++) {
+            if (strcmp(b[i].nom, b[j].nom) > 0) {
                 temp = b[i];
                 b[i] = b[j];
                 b[j] = temp;
             }
         }
     }
-    printf("les liste des etudiantes en ordre alphabetique :\n");
+    printf("√âtudiants par ordre alphab√©tique :\n");
+    for (int i = 0; i < dim; i++) {
+        printf("%s %s : %.2f\n", b[i].nom, b[i].prenom, b[i].note_generale);
+    }
+}
+
+void trier_par_note() {
+    etudiant b[100000];
+    for (int i = 0; i < dim - 1; i++) 
+    b[i]=t[i];
+    etudiant temp;
     for (int i = 0; i < dim - 1; i++) {
-        printf("%s\t%s\t%d/%d/%d\t%f",t[i].Nom,t[i].prenom,t[i].date.anee,t[i].date.mois,t[i].date.jour,t[i].Note_gÈnÈrale);}
+        for (int j = i + 1; j < dim; j++) {
+            if (b[i].note_generale < b[j].note_generale) {
+                temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+    printf("√âtudiants tri√©s par note (de la plus haute √† la plus basse) :\n");
+    for (int i = 0; i < dim; i++) {
+        printf("%s %s : %.2f\n", b[i].nom, b[i].prenom, b[i].note_generale);
+    }
 }
 
-void trie(){
-
-
+void trier_par_statut() {
+    etudiant b[100000];
+    memcpy(b, t, sizeof(t));
+    etudiant temp;
+    int count = 0;
+    for (int i = 0; i < dim - 1; i++) {
+        for (int j = i + 1; j < dim; j++) {
+            if (b[i].note_generale < b[j].note_generale) {
+                temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+    printf("√âtudiants ayant r√©ussi (note >= 10) :\n");
+    for (int i = 0; i < dim; i++) {
+        if (b[i].note_generale >= 10) {
+            printf("%s %s : %.2f\n", b[i].nom, b[i].prenom, b[i].note_generale);
+        }
+    }
 }
-int main(){
+
+void menu() {
+    printf("****************Menu :****************\n");
+    printf("\t\t**** Liste des choix ****\n");
+    printf("\t\t1. Ajouter √©tudiant\n");
+    printf("\t\t2. Ajouter d√©partement\n");
+    printf("3. Modifier les informations pour un √©tudiant\n");
+    printf("4. Supprimer les informations pour un √©tudiant\n");
+    printf("5. Afficher les informations d'un √©tudiant\n");
+    printf("6. Calculer la moyenne generale pour un d√©partement\n\t\t\t et pour l'universit√©\n");
+    printf("7. Afficher les statistiques de l'universit√©\n");
+    printf("\t\t\t8. Chercher un √©tudiant\n");
+    printf("9. Afficher les etudiants par ordre alphabetique\n");
+    printf("10. Trier les etudiants par moyenne generale\n");
+    printf("\t11. Afficher le statut de reussite\n");
+    printf("\t\t\t   12. Quitter\n");
+    printf("\t\tEntrez un choix pour continuer :\n");
+    printf("****************************************\n");
+}
+
+int main() {
+    int choix;
+    do {
+        menu();
+        scanf("%d", &choix);
+        switch (choix) {
+            case 1:
+                ajouter_etud();
+                break;
+            case 2:
+                ajouter_dep();
+                break;
+            case 3:
+                modifier_info();
+                break;
+            case 4:
+                supprimer_info();
+                break;
+            case 5:
+                afficher_info();
+                break;
+            case 6:
+                calcule_moyenne();
+                break;
+            case 7:
+                afficher_statistiques();
+                break;
+            case 8:
+                chercher();
+                break;
+            case 9:
+                afficher_par_alpha();
+                break;
+            case 10:
+                trier_par_note();
+                break;
+            case 11:
+                trier_par_statut();
+                break;
+            case 12:
+                printf("Vous avez quitt√© le programme. √Ä la prochaine fois !\n");
+                break;
+            default:
+                printf("Choix invalide !\n");
+                break;
+        }
+    } while (choix != 12);
     return 0;
 }
